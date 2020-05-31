aaca57c5cbc259cc009c1a669b2dea7dc6a9ba46
ohsuyuki <papu8osu@gmail.com>
2020-06-01 00:05:56 +0900

    initial commit

--- AtCoderABC169B/main.cpp
+++ AtCoderABC169B/main.cpp
@@ -1,15 +1,36 @@
-//
-//  main.cpp
-//  AtCoderABC169B
-//
-//  Created by yuuki oosu on 2020/05/31.
-//  Copyright © 2020 yuuki oosu. All rights reserved.
-//
-
 #include <iostream>
+#include <cmath>
+
+long long MAX = std::pow(10, 18);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    long long N = 0;
+    scanf("%lld", &N);
+
+    long long ans = 0;
+    scanf("%lld", &ans);
+
+    for(int i=1; i<N; i++) {
+        long long num = 0;
+        scanf("%lld", &num);
+
+        if(ans == -1 && num == 0) {
+            ans = 0;
+        } else if(ans == -1 && num > 0) {
+            ans = -1;
+        } else if(MAX/ans < num) {
+            ans = -1;
+        } else {
+            ans *= num;
+        }
+
+        if(ans == 0) {
+            break;
+        }
+    }
+
+    std::cout << ans;
+
     return 0;
 }
+
